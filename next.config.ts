33003a623a4da672d0861828f897759d7b7f8b91
lovemage <lovemage@gmail.com>
2025-11-21 14:57:08 +0800

    Remove empty webpack config to fix Turbopack conflict

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  webpack: (config) => {
-    return config;
-  },
 };
 
 export default nextConfig;
